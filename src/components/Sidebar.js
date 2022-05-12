4ff4b03ee0678496bd9b05771e29b2fc415e30e3
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-05-12 09:01:39 +0530

    Added shopping cart using React-redux

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,40 @@
-function Sidebar({ products }) {
-  let sizes = products.reduce((acc, cv) => {
+import React from 'react';
+import { connect } from 'react-redux';
+import { handleSelectedSize } from '../store/action';
+
+function Sidebar(props) {
+  function handleSize(val) {
+    props.dispatch(handleSelectedSize(val));
+  }
+
+  let sizes = props.products.reduce((acc, cv) => {
     acc = acc.concat(cv.availableSizes);
     return acc;
   }, []);
   let uniqueSizes = [...new Set(sizes)];
   return (
-    <aside className="flex-20 sidebar">
-      <div className="flex wrap">
+    <aside className='flex-20 sidebar'>
+      <div className='flex wrap'>
         {uniqueSizes.map((size) => (
-          <span className="size">{size}</span>
+          <span
+            key={size}
+            className={
+              props.state.selectedSize.includes(size) ? 'active size' : 'size'
+            }
+            onClick={() => handleSize(size)}
+          >
+            {size}
+          </span>
         ))}
       </div>
     </aside>
   );
 }
 
-export default Sidebar;
+function mapStateToProps(state) {
+  return {
+    state,
+  };
+}
+
+export default connect(mapStateToProps)(Sidebar);
