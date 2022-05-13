8001a40f09b03faa997b72b5fc947a9792b4e714
Piyush Chamoli <piyushchamoli11@gmail.com>
2022-05-14 00:02:54 +0530

    redux refactor

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,23 +1,17 @@
-import React from 'react';
+import React from "react";
 
 function CartItem(props) {
   return (
-    <div className='cart-item'>
-      <img
-        src='/static/products/876661122392077_2.jpg'
-        alt=''
-        width='80'
-      />
-      <div className='cart-item-details'>
-        <p className='cart-item-name'>
-          Sphynx Tie Dye Wine T-Shirt
-        </p>
+    <div className="cart-item">
+      <img src="/static/products/876661122392077_2.jpg" alt="" width="80" />
+      <div className="cart-item-details">
+        <p className="cart-item-name">Sphynx Tie Dye Wine T-Shirt</p>
         <p>X | Front tie dye</p>
         <p>print Quantity: 1</p>
       </div>
-      <div className='cart-price'>
-        <p className='cart-cross'>x</p>
-        <p className='price'>$ 19.00</p>
+      <div className="cart-price">
+        <p className="cart-cross">x</p>
+        <p className="price">$ 19.00</p>
         <div>
           <Increment />
           <Decrement />
@@ -30,17 +24,17 @@ function CartItem(props) {
 function Increment() {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
-      className='plus-icon'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="plus-icon"
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={2}
-        d='M12 6v6m0 0v6m0-6h6m-6 0H6'
+        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
       />
     </svg>
   );
@@ -48,17 +42,17 @@ function Increment() {
 function Decrement() {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
-      className='plus-icon'
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      className="plus-icon"
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
+        strokeLinecap="round"
+        strokeLinejoin="round"
         strokeWidth={2}
-        d='M18 12H6'
+        d="M18 12H6"
       />
     </svg>
   );
---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from "redux";
+import data from "../data.json";
+
+let products = data.products;
+
+function Reducer(state = { isOpen: false, products: [] }, action) {
+  switch (action.type) {
+    case "highest":
+      state.products = [...products].sort((a, b) => b.price - a.price);
+      return { ...state, products: state.products };
+    case "lowest":
+      state.products = [...products].sort((a, b) => a.price - b.price);
+      return { ...state, products: state.products };
+    case "isOpen":
+      state.isOpen = action.payload;
+      return { ...state, isOpen: state.isOpen };
+    default:
+      state.products = [...products];
+      return { ...state, products: state.products };
+  }
+}
+
+let store = createStore(Reducer);
+
+export default store;
