0048cc6f14963a33638711945e1d7f5da9f855ad
raj shringi <raj24shringi@gmail.com>
2022-11-24 16:05:52 +0530

    handle add item to cart, remove item, increase and decrease quantity and get subtotal of cart

--- src/store/actions.js
+++ src/store/actions.js
@@ -4,3 +4,31 @@ export default function selectSize(size) {
     payload: size,
   };
 }
+
+export function addProductToCart(product) {
+  return {
+    type: "add_product",
+    payload: product,
+  };
+}
+
+export function incrementQuantity(id) {
+  return {
+    type: "increment_quantity",
+    payload: id,
+  };
+}
+
+export function decrementQuantity(id) {
+  return {
+    type: "decrement_quantity",
+    payload: id,
+  };
+}
+
+export function removeItem(id) {
+  return {
+    type: "remove_item",
+    payload: id,
+  };
+}
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,18 @@ const initialState = {
   cart: [],
 };
 
+function incQuantity(state, id) {
+  return {
+    ...state,
+    cart: state.cart.map((product) => {
+      if (product.id === id) {
+        product.quantity++;
+      }
+      return product;
+    }),
+  };
+}
+
 function reducer(state = initialState, action) {
   switch (action.type) {
     case "select_size":
@@ -14,6 +26,30 @@ function reducer(state = initialState, action) {
             sizes: state.sizes.filter((size) => size !== action.payload),
           }
         : { ...state, sizes: [...state.sizes, action.payload] };
+    case "add_product":
+      if (state.cart.some((product) => product.id === action.payload.id)) {
+        return incQuantity(state, action.payload.id);
+      }
+      return { ...state, cart: [...state.cart, action.payload] };
+
+    case "remove_item":
+      return {
+        ...state,
+        cart: state.cart.filter((product) => product.id !== action.payload),
+      };
+    case "increment_quantity":
+      return incQuantity(state, action.payload);
+
+    case "decrement_quantity":
+      return {
+        ...state,
+        cart: state.cart.map((product) => {
+          if (product.id === action.payload) {
+            product.quantity--;
+          }
+          return product;
+        }),
+      };
     default:
       return state;
   }
